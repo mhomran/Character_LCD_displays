48af0ec922fe58bac6b23d72acad6025cf507bd1
Mohamed Hassanin Omran <mohamed_hassanin_omran@yahoo.com>
2021-03-31 00:18:01 +0200

    Add: configuration source file.

---
+++ src/lcd_display_cfg.c
@@ -0,0 +1,45 @@
+/**
+ * @file lcd_display_cfg.c
+ * @author Mohamed Hassanin
+ * @brief Character LCD displays pattern for time-triggered embedded systems.
+ * @version 0.1
+ * @date 2021-03-31
+ */
+
+
+/******************************************************************************
+* Includes
+******************************************************************************/
+#include "lcd_display_cfg.h"
+
+/******************************************************************************
+* Module Variable Definitions
+******************************************************************************/
+static LcdDisplayConfig_t LcdDisplayConfig[LCD_DISPLAY_MAX] = {
+  {
+    .Display = LCD_DISPLAY_0,
+    .En = PORTA_0,
+    .Rs = PORTA_1,
+    .Data =
+    {
+      PORTA_2,
+      PORTA_3,
+      PORTA_4,
+      PORTA_5
+    }
+  }
+};
+
+/******************************************************************************
+* Function Definitions
+******************************************************************************/
+/**
+ * @brief return the configuration structure holding the 
+ * configuration table of the module
+ */
+LcdDisplayConfig_t* const
+LcdDisplay_GetConfig(void)
+{
+  return LcdDisplayConfig;
+}
+/***************************** END OF FILE ***********************************/
